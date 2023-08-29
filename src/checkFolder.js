637f003668eb07f9f8599bafe586750a678158d5
roman <roman.rubinshtein@1win.pro>
2023-08-30 00:40:34 +0300

    chamges

--- src/checkFolder.js
+++ src/checkFolder.js
@@ -7,13 +7,13 @@ async function checkFolder() {
     await fs.access('src/links.json');
   } catch (err) {
     console.log(err.message);
-    const data = await axios.get('http://halooglasi.com/nekretnine/izdavanje-stanova/beograd?oglasivac_nekretnine_id_l=387237%2C387300');
+    const data = await axios.get('https://www.halooglasi.com/nekretnine/izdavanje-stanova/beograd?oglasivac_nekretnine_id_l=387237%2C387300');
     const html = data.data;
     const regex = /<h3 class="product-title">.*?<a\s+href="([^"]+)"/g;
     const matches = [...html.matchAll(regex)];
 
     if (matches.length > 0) {
-      const links = matches.map((match) => `http://halooglasi.com${match[1]}`);
+      const links = matches.map((match) => `https://www.halooglasi.com${match[1]}`);
       const jsonData = JSON.stringify(links, null, 2);
 
       await fs.writeFile('src/links.json', jsonData, 'utf8');
