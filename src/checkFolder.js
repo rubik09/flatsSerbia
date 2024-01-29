1a1e96e0d4abe87b1ef6fb09c6f788fc6d14a4a2
Artem Koludarov <artemkoludarov@MacBook-Air-Artem.local>
2024-01-29 22:09:34 +0100

    added 5 page to HaloOglasi

--- src/checkFolder.js
+++ src/checkFolder.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import fs from 'fs/promises';
-import { regexSearchNewFlats } from './config';
+import { regexSearchNewFlats, PAGES_HALOOGLASI } from './config';
 // {/* <h2 class="offer-title text-truncate w-100">…</h2> */}
 async function checkFolder() {
   try {
@@ -21,16 +21,20 @@ async function checkFolder() {
     } else {
       console.log('Ссылки с классом offer-title не найдены.');
     }
+    const mainLink = 'https://www.halooglasi.com/nekretnine/izdavanje-stanova/beograd?oglasivac_nekretnine_id_l=387237%2C387300';
+    let newLinks = [];
+    for(let i = 1; i < PAGES_HALOOGLASI + 1; i++){
+      const { data } = await axios.get(`${mainLink}&page=${i}`);
+      const regexHalo1 = /<h3 class="product-title">.*?<a\s+href="([^"]+)"/g;
+      const matchesHalo1 = [...data.matchAll(regexHalo1)];
+  
+      if (matchesHalo1.length > 0) {
+        const linksHalo = matchesHalo1.map((match) => `https://www.halooglasi.com${match[1]}`);
+      newLinks = [...newLinks, ...linksHalo]
+    }}
 
-    const dataHalo = await axios.get('https://www.halooglasi.com/nekretnine/izdavanje-stanova/beograd?oglasivac_nekretnine_id_l=387237%2C387300');
-    const htmlHalo = dataHalo.data;
-    const regexHalo = /<h3 class="product-title">.*?<a\s+href="([^"]+)"/g;
-    const matchesHalo = [...htmlHalo.matchAll(regexHalo)];
-
-    if (matchesHalo.length > 0) {
-      const links = matchesHalo.map((match) => `https://www.halooglasi.com${match[1]}`);
-      const jsonData = JSON.stringify(links, null, 2);
-
+    if (newLinks.length > 0) {
+      const jsonData = JSON.stringify(newLinks, null, 2);
       await fs.writeFile('src/links.json', jsonData, 'utf8');
       console.log('create file links.json');
     } else {
